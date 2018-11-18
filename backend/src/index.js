66b22437622ab5baabdd592590434a1deb6ec2ed
Iannes <js15js@gmail.com>
2018-11-18 15:35:49 +0000

    Feat: Add Yoga server config

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,22 @@
-// let's go!
+
+require('dotenv').config({ path:'./.env'});
+
+const createServer = require('./createServer');
+const db = require('./db');
+
+const server = createServer();
+
+// Use express middleware to handle cookies
+
+// Use express middleware to populate current user
+
+
+// Spin up the server
+server.start({
+    cors: {
+        credentials: true,
+        origin: process.env.FRONTEND_URL
+    }
+}, deets => {
+    console.log(`Server is now running on port http://localhost:${deets.port}`);
+});
